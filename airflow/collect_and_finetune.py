43161d913ab4cf38de3b0280bb366ab9799af5cb
Sergey Karatach <karatach1998@yandex.ru>
2023-04-25 22:24:51 +0300

    Fix

--- airflow/collect_and_finetune.py
+++ airflow/collect_and_finetune.py
@@ -198,10 +198,10 @@ with DAG(dag_id="collect_and_finetune", start_date=datetime.now(), schedule="*/2
             raise AirflowFailException()
     
     @task
-    def create_scrapper_worker(conn):
+    def create_scrapper_worker(**kwargs):
         config.load_incluster_config()
         core_api = client.CoreV1Api()
-        core_api.create_namespaced_pod(body=scrapper_worker_pod(conn), namespace=kube_namespace)
+        core_api.create_namespaced_pod(body=scrapper_worker_pod(kwargs.get('conn')), namespace=kube_namespace)
 
     @task
     def delete_selenium_hub():
@@ -218,10 +218,10 @@ with DAG(dag_id="collect_and_finetune", start_date=datetime.now(), schedule="*/2
         client.AppsV1Api().delete_namespaced_deployment(body=selenium_node_deployment, namespace=kube_namespace)
     
     @task
-    def delete_scrapper_worker(conn):
+    def delete_scrapper_worker(**kwargs):
         config.load_incluster_config()
         core_api = client.CoreV1Api()
-        core_api.delete_namespaced_pod(body=scrapper_worker_pod(conn), namespace=kube_namespace)
+        core_api.delete_namespaced_pod(body=scrapper_worker_pod(kwargs.get('conn')), namespace=kube_namespace)
 
     scrapper_producer = KubernetesPodOperator(
         task_id="scrape_offers_list",
