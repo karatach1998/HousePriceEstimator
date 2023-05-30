d4101e7b5e1017fcfefc5b9493b679bd8388cac3
Sergey Karatach <karatach1998@yandex.ru>
2023-05-30 12:22:20 +0300

    Fix

--- cian_scrapper/cian_scrapper/main.py
+++ cian_scrapper/cian_scrapper/main.py
@@ -90,7 +90,6 @@ async def _publish_result(result, queue_name):
     )
     async with connection:
         channel = await connection.channel()
-        print("RESULT", result)
         exchange = await channel.declare_exchange(queue_name, durable=True)
         queue = await channel.declare_queue(queue_name, durable=True)
         await queue.bind(exchange)
